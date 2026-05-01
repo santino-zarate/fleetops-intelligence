4e723af7e117c9478ca1e3779c2a0f96aa131461
Santio <zaratesantino41@gmail.com>
2026-05-01 20:57:36 -0300

    chore: initialize FleetOps Intelligence project

---
+++ src/main/java/com/fleetops/FleetopsIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.fleetops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetopsIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetopsIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetops/FleetopsIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetopsIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
